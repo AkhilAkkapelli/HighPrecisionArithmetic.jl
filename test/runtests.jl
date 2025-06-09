1da3523f10775be36c3ae2d94724f94957b6b398
Akhil Akkapelli <41839847+AkhilAkkapelli@users.noreply.github.com>
2025-06-09 12:22:28 +0530

    Update runtests.jl

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,10 +1,6 @@
-using Pkg
 using Test
 
 push!(LOAD_PATH, joinpath(@__DIR__, "..", "src"))
-
-Pkg.activate(joinpath(@__DIR__, ".."))
-Pkg.instantiate()
 
 @testset "HighPrecision Library Tests" begin
   
