17734b906e0bcb0af545a6e6ebd769b0388f6934
Akhil Akkapelli <41839847+AkhilAkkapelli@users.noreply.github.com>
2025-06-14 19:17:56 +0530

    Update make.jl

--- docs/make.jl
+++ docs/make.jl
@@ -17,7 +17,7 @@ makedocs(
     ),
     pages = [
         "Home" => "index.md",
-        "High Precision Number" => "HighPrecisionNumber.md",
+        "High Precision Integer" => "HighPrecisionInteger.md",
         "High Precision Linear Algebra" => "HighPrecisionLinearAlgebra.md",
         "API" => "api/high-precision-arithmetic.md",
     ],
