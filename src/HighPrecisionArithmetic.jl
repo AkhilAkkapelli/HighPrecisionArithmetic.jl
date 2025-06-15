dfa1b328ada9681b3de64cefedb220ff464cabed
Akhil Akkapelli <41839847+AkhilAkkapelli@users.noreply.github.com>
2025-06-15 10:45:42 +0530

    Update HighPrecisionArithmetic.jl

--- src/HighPrecisionArithmetic.jl
+++ src/HighPrecisionArithmetic.jl
@@ -1,6 +1,7 @@
 module HighPrecisionArithmetic
 
   export HIGH_PRECISION_BASE, HighPrecisionInt, @hpi_str
+  export HighPrecisionVector, HighPrecisionMatrix
 
   include("HighPrecisionInteger.jl")   
   include("HighPrecisionLinearAlgebra.jl")
